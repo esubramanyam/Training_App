1a2b41e956815638b42108c1bd5e201948848122
jeyashree subramanian <jesubramaniam@deloitte.com>
2025-06-16 17:55:00 +0530

    Initial Commit

--- src/main/java/com/example/FIS_EmployerService/FisEmployeeServiceApplication.java
+++ src/main/java/com/example/FIS_EmployerService/FisEmployeeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.FIS_EmployeeService;
+package com.example.FIS_EmployerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
