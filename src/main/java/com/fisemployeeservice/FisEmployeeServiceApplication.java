a0d1e2ebb17f6e6154122aa37388032f71b3cb35
Tej Kumar, Sunkara <ttejkumar@deloitte.com>
2025-06-16 19:28:30 +0530

    added model pkg and class

--- src/main/java/com/fisemployeeservice/FisEmployeeServiceApplication.java
+++ src/main/java/com/fisemployeeservice/FisEmployeeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.FIS_EmployeeService;
+package com.fisemployeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
