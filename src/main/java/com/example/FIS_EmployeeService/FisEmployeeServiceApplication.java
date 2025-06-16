6ef8eafbc2045a634cdfc0f0e5d226e60ec97efe
jeyashree subramanian <jesubramaniam@deloitte.com>
2025-06-16 15:48:42 +0530

    Initial Commit

---
+++ src/main/java/com/example/FIS_EmployeeService/FisEmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.FIS_EmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FisEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FIS_EmployeeService/FisEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FIS_EmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
